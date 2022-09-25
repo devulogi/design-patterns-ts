7d282be0114d62175e8736e8a8fb376e5dd4098b
devulogi <devulogijrp.github@gmail.com>
2022-09-25 16:56:49 +0800

    ammended comments

--- creational-patterns/abstract-factory.ts
+++ creational-patterns/abstract-factory.ts
@@ -126,9 +126,9 @@ export class PokemonTrainer extends Trainer {
 }
 
 /**
- * EN: The client code works with factories and products only through abstract
- * types: AbstractFactory and AbstractProduct. This lets you pass any factory or
- * product subclass to the client code without breaking it.
+ * The client code works with factories only through abstract
+ * types: AbstractFactory. This lets you pass any factory or
+ * IPokemon subclass to the client code without breaking it.
  * @param factory {ITrainerAbstractFactory}
  * @return Trainer {GymLeader | PokemonTrainer}
  */
